480cc897eb927876e06c44beef6ed511ea81ed82
OZ <omarzorrillallerena@gmail.com>
2024-03-22 03:06:27 -0500

    Custom theme

---
+++ web/themes/custom/desafio2/js/desafio2.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * desafio2 behaviors.
+ */
+(function (Drupal) {
+
+    'use strict';
+
+    Drupal.behaviors.desafio2 = {
+        attach(context, settings) {
+
+            console.log('It works!');
+
+        }
+    };
+
+} (Drupal));
